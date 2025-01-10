ae652ae1ffc7351b382ad3fa429071c87a9d0f8d
Johann Goncalves Pereira <johann.gon.pereira@gmail.com>
2025-01-10 20:57:24 -0300

    feat: tailwind

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,9 @@
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev()],
+  integrations: [qwikdev(), tailwind()],
 });
