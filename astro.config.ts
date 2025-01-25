e405c5c9ef16b680bbf1b205320a05a2d0c5882a
Johann Goncalves Pereira <johann.gon.pereira@gmail.com>
2025-01-25 12:38:31 -0300

    feat: tests

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,16 @@
 import qwikdev from "@qwikdev/astro";
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev(), tailwind()],
+  integrations: [qwikdev(), tailwind(), partytown()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  prefetch: true,
 });
